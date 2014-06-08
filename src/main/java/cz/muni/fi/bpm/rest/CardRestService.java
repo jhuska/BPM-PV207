06158ff55299f5c1acca955f5a0226a99ed79f93
Ron Smeral <rsmeral@redhat.com>
2014-06-08 02:47:53 +0200

    Implemented services, added response wrappers for JAXB, fixed params

--- src/main/java/cz/muni/fi/bpm/rest/CardRestService.java
+++ src/main/java/cz/muni/fi/bpm/rest/CardRestService.java
@@ -1,29 +1,59 @@
 package cz.muni.fi.bpm.rest;
 
-import cz.muni.fi.bpm.managers.BillManager;
 import cz.muni.fi.bpm.model.Card;
+import java.util.Set;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.POST;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
 
 /**
  *
  * @author jhuska
  */
-@Path("/cards")
+@Path("/card")
 @RequestScoped
 public class CardRestService {
 
-    @Inject
-    private BillManager billManager;
-    
-    @POST
+    @XmlRootElement
+    public static class CardValid {
+
+        @XmlValue
+        public boolean valid;
+
+        public CardValid() {
+        }
+
+        public CardValid(boolean valid) {
+            this.valid = valid;
+        }
+    }
+
+    @PUT
     @Path("/isValid")
     @Produces(MediaType.APPLICATION_XML)
-    public boolean isCardValid(Card card) {
-        return false;
+    public CardValid isCardValid(
+            @FormParam("cardNr") String cardNumber,
+            @FormParam("cvv") String cvv,
+            @FormParam("validityMonth") String validityMonth,
+            @FormParam("validityYear") String validityYear) {
+
+        // validate the passed parameters using Bean Validation
+        Card card = new Card(cardNumber, cvv, validityMonth, validityYear);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Card>> violations = validator.validate(card);
+
+        return new CardValid(violations.isEmpty());
+
     }
+
 }
--- src/main/java/cz/muni/fi/bpm/rest/DeliveryService.java
+++ src/main/java/cz/muni/fi/bpm/rest/DeliveryService.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
 
 /**
  *
@@ -14,12 +16,26 @@ import javax.ws.rs.core.MediaType;
 @Path("/delivery")
 @RequestScoped
 public class DeliveryService {
-    
+
+    @XmlRootElement
+    public static class OrderDelivered {
+
+        @XmlValue
+        public boolean delivered;
+
+        public OrderDelivered() {
+        }
+
+        public OrderDelivered(boolean valid) {
+            this.delivered = valid;
+        }
+    }
+
     @GET
     @Path("/{id:[0-9][0-9]*}")
     @Produces(MediaType.APPLICATION_XML)
-    public boolean isOrderDelivered(@PathParam("id") long orderId) {
-        return false;
+    public OrderDelivered isOrderDelivered(@PathParam("id") long orderId) {
+        return new OrderDelivered(orderId % 2 == 0);
     }
-    
+
 }
